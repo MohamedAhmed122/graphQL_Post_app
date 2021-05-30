0b612bd51607c6f7a84cffb1cb49253c6ed05253
MohamedAhmed122 <mohamed.ahm9833@gmail.com>
2021-05-30 16:00:17 +0700

    Structure the App

---
+++ App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import {StyleSheet, SafeAreaView, Text, View} from 'react-native';
+
+export default function App() {
+  return (
+    <View>
+      <SafeAreaView />
+      <Text>Hello, World</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({});
---
+++ src/config/colors.js
@@ -0,0 +1,17 @@
+export const primary = '#EAC74D';
+
+export const secondary = '#1976D2';
+
+export const blue = '#18B5D9';
+
+export const danger = '#FF685C';
+
+export const dark = '#000';
+
+export const lightGray = '#eaeaea';
+
+export const darkGray = '#6e6969';
+
+export const white = '#fff';
+
+export const gray = '#EFEFEF';
